bd076f70eee4e02f49c75b5fdb1e0cca87a03cf9
mgrempel <57542255+mgrempel@users.noreply.github.com>
2020-11-16 20:25:15 -0600

    Added create account activity

---
+++ app/src/main/java/com/pixelworks/roam/CreateAccountActivity.java
@@ -0,0 +1,82 @@
+package com.pixelworks.roam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.apollographql.apollo.ApolloCall;
+import com.apollographql.apollo.ApolloClient;
+import com.apollographql.apollo.api.Response;
+import com.apollographql.apollo.exception.ApolloException;
+
+import org.jetbrains.annotations.NotNull;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    private EditText editTextUserName, editTextPassword, editTextEmail, editTextFirstName, editTextLastName, editTextDescription;
+    private Button btnCreateAccount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+
+        //Let's find our controls
+        editTextUserName = findViewById(R.id.editTextDescription);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextFirstName = findViewById(R.id.editTextFirstName);
+        editTextLastName = findViewById(R.id.editTextLastName);
+        editTextDescription = findViewById(R.id.editTextDescription);
+
+        btnCreateAccount = findViewById(R.id.btnCreateAccount);
+
+        ApolloClient apolloClient = ApolloClient.builder()
+                .serverUrl("http://10.0.2.2:8080/query")
+                .build();
+
+        //Let's handle our click event
+        btnCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Ensure all necessary fields are populated
+
+                //Create our GraphQL request
+                CreateUserMutation createUserMutation = CreateUserMutation.builder()
+                        .userName(editTextUserName.getText().toString())
+                        .password(editTextPassword.getText().toString())
+                        .email(editTextEmail.getText().toString())
+                        .firstName(editTextFirstName.getText().toString())
+                        .lastName(editTextLastName.getText().toString())
+                        .description(editTextDescription.getText().toString())
+                        .build();
+
+                //Let's ring up the GQL server
+                apolloClient
+                        .mutate(createUserMutation)
+                        .enqueue(
+                                new ApolloCall.Callback<CreateUserMutation.Data>() {
+                                    @Override
+                                    public void onResponse(@NotNull Response<CreateUserMutation.Data> response) {
+                                        int result = response.getData().createUser().id();
+                                        Log.d("TEST", String.valueOf(result));
+                                    }
+
+                                    @Override
+                                    public void onFailure(@NotNull ApolloException e) {
+                                        //Toast.makeText(MainActivity.this, "It didn't work!", Toast.LENGTH_LONG).show();
+                                        Log.d("TEST", e.getCause().toString());
+                                    }
+                                }
+                        );
+
+                //Handle request to determine validity of response.
+            }
+        });
+    }
+}
--- app/src/main/java/com/pixelworks/roam/MainActivity.java
+++ app/src/main/java/com/pixelworks/roam/MainActivity.java
@@ -2,81 +2,28 @@ package com.pixelworks.roam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import com.apollographql.apollo.ApolloCall;
-import com.apollographql.apollo.ApolloClient;
-import com.apollographql.apollo.api.Response;
-import com.apollographql.apollo.exception.ApolloException;
-
-import org.jetbrains.annotations.NotNull;
 
 public class MainActivity extends AppCompatActivity {
-
-    private EditText editTextUserName, editTextPassword, editTextEmail, editTextFirstName, editTextLastName, editTextDescription;
-    private Button btnCreateAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Let's find our controls
-        editTextUserName = findViewById(R.id.editTextDescription);
-        editTextPassword = findViewById(R.id.editTextPassword);
-        editTextEmail = findViewById(R.id.editTextEmail);
-        editTextFirstName = findViewById(R.id.editTextFirstName);
-        editTextLastName = findViewById(R.id.editTextLastName);
-        editTextDescription = findViewById(R.id.editTextDescription);
+        //Lets check and see if the user has a token.
+        SharedPreferences sharedPreferences = getSharedPreferences("roam", Context.MODE_PRIVATE);
+        String uuid = sharedPreferences.getString("sessionKey", "NOKEY");
 
-        btnCreateAccount = findViewById(R.id.btnCreateAccount);
+        if(uuid.equals("NOKEY")) {
+            //Redirect user to sign in page
 
-        ApolloClient apolloClient = ApolloClient.builder()
-                .serverUrl("http://10.0.2.2:8080/query")
-                .build();
-
-        //Let's handle our click event
-        btnCreateAccount.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Ensure all necessary fields are populated
-
-                //Create our GraphQL request
-                CreateUserMutation createUserMutation = CreateUserMutation.builder()
-                        .userName(editTextUserName.getText().toString())
-                        .password(editTextPassword.getText().toString())
-                        .email(editTextEmail.getText().toString())
-                        .firstName(editTextFirstName.getText().toString())
-                        .lastName(editTextLastName.getText().toString())
-                        .description(editTextDescription.getText().toString())
-                        .build();
-
-                //Let's ring up the GQL server
-                apolloClient
-                        .mutate(createUserMutation)
-                        .enqueue(
-                                new ApolloCall.Callback<CreateUserMutation.Data>() {
-                                    @Override
-                                    public void onResponse(@NotNull Response<CreateUserMutation.Data> response) {
-                                        int result = response.getData().createUser().id();
-                                        Log.d("TEST", String.valueOf(result));
-                                    }
-
-                                    @Override
-                                    public void onFailure(@NotNull ApolloException e) {
-                                        //Toast.makeText(MainActivity.this, "It didn't work!", Toast.LENGTH_LONG).show();
-                                        Log.d("TEST", e.getCause().toString());
-                                    }
-                                }
-                        );
-
-                //Handle request to determine validity of response.
-            }
-        });
+        }
+        else {
+            //Let's hit the database and grab the users information.
+        }
     }
 }
