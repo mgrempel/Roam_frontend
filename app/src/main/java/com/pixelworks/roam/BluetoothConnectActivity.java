cc2454f5af5851132aa7a27a88b2d1025a36bd8e
mgrempel <57542255+mgrempel@users.noreply.github.com>
2020-11-25 20:47:40 -0600

    Created viewposts and bluetooth connect activity

---
+++ app/src/main/java/com/pixelworks/roam/BluetoothConnectActivity.java
@@ -0,0 +1,14 @@
+package com.pixelworks.roam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BluetoothConnectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bluetooth_connect);
+    }
+}
