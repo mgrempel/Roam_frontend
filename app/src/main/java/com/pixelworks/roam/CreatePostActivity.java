b4e37af01c2753b40703a26b440241f0eb69881c
mgrempel <57542255+mgrempel@users.noreply.github.com>
2020-11-25 20:42:34 -0600

    Implemented create post API call

--- app/src/main/java/com/pixelworks/roam/CreatePostActivity.java
+++ app/src/main/java/com/pixelworks/roam/CreatePostActivity.java
@@ -8,6 +8,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.apollographql.apollo.ApolloCall;
+import com.apollographql.apollo.ApolloClient;
+import com.apollographql.apollo.api.Response;
+import com.apollographql.apollo.exception.ApolloException;
+
+import org.jetbrains.annotations.NotNull;
+
 public class CreatePostActivity extends AppCompatActivity {
 
     EditText txtTitle, txtContent;
@@ -32,8 +39,38 @@ public class CreatePostActivity extends AppCompatActivity {
                 }
                 else {
                     //Create our query
+                    ApolloClient apolloClient = ApolloClient.builder()
+                            .serverUrl(getString(R.string.api_location))
+                            .build();
+
+                    //Create our graphql request
+                    CreatePostMutation createPostMutation = CreatePostMutation.builder()
+                            .title(txtTitle.getText().toString())
+                            .content(txtContent.getText().toString())
+                            .uuid(SharedPreferencesHelper.getStringValue("uuid"))
+                            .build();
 
                     //Shoot query up to the API
+                    apolloClient
+                            .mutate(createPostMutation)
+                            .enqueue(
+                                    new ApolloCall.Callback<CreatePostMutation.Data>() {
+                                        @Override
+                                        public void onResponse(@NotNull Response<CreatePostMutation.Data> response) {
+                                            finish();
+                                        }
+
+                                        @Override
+                                        public void onFailure(@NotNull ApolloException e) {
+                                            CreatePostActivity createPostActivity = CreatePostActivity.this;
+                                            createPostActivity.runOnUiThread(new Runnable() {
+                                                public void run() {
+                                                    Toast.makeText(createPostActivity, "An error has occurred!", Toast.LENGTH_SHORT).show();
+                                                }
+                                            });
+                                        }
+                                    }
+                            );
                 }
             }
         });
