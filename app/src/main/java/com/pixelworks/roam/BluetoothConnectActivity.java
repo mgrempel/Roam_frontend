79e8bd2571af2b8023e96af4b6c36fb70278d2d2
mgrempel <57542255+mgrempel@users.noreply.github.com>
2020-11-26 18:33:51 -0600

    Implemented bluetooth check

--- app/src/main/java/com/pixelworks/roam/BluetoothConnectActivity.java
+++ app/src/main/java/com/pixelworks/roam/BluetoothConnectActivity.java
@@ -1,14 +1,45 @@
 package com.pixelworks.roam;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class BluetoothConnectActivity extends AppCompatActivity {
+
+    private BluetoothAdapter bluetoothAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth_connect);
+
+        //Get our bluetooth object
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(bluetoothAdapter == null) {
+            //Device doesn't have bluetooth, we're done here.
+            finish();
+        }
+
+        //Turn on the adapter
+        if(!bluetoothAdapter.isEnabled()) {
+            Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBluetooth, 1);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //Handle callback for turning on bluetooth.
+        if(requestCode == 1) {
+            if(resultCode != RESULT_OK) {
+                finish();
+            }
+        }
     }
 }
