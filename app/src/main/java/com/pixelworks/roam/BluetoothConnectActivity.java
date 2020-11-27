20a66a2cde0e613827d093dccb4b9ffd8e9a34cc
mgrempel <57542255+mgrempel@users.noreply.github.com>
2020-11-26 19:53:29 -0600

    Added location permission check and request

--- app/src/main/java/com/pixelworks/roam/BluetoothConnectActivity.java
+++ app/src/main/java/com/pixelworks/roam/BluetoothConnectActivity.java
@@ -2,15 +2,25 @@ package com.pixelworks.roam;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class BluetoothConnectActivity extends AppCompatActivity {
 
     private BluetoothAdapter bluetoothAdapter;
+    private LocationManager locationManager;
+
+    final private int REQUEST_CODE_ASK_PERMISSIONS = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +39,30 @@ public class BluetoothConnectActivity extends AppCompatActivity {
             Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBluetooth, 1);
         }
+
+        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        if(locationManager == null) {
+            //Device doesn't have a GPS
+            finish();
+        }
+
+        //Request location permissions
+        if ( Build.VERSION.SDK_INT >= 23){
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) !=
+                    PackageManager.PERMISSION_GRANTED  ){
+                requestPermissions(new String[]{
+                                android.Manifest.permission.ACCESS_FINE_LOCATION},
+                        REQUEST_CODE_ASK_PERMISSIONS);
+                return ;
+            }
+        }
+
+        //turn on location
+        if(!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Intent enableLocation = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(enableLocation);
+        }
+
     }
 
     @Override
@@ -40,6 +74,22 @@ public class BluetoothConnectActivity extends AppCompatActivity {
             if(resultCode != RESULT_OK) {
                 finish();
             }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_PERMISSIONS:
+                if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    finish();
+                }
+                else {
+                    
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
