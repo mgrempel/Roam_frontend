ef8dfec69e480ddb0310a646ccadca4add74def3
mgrempel <57542255+mgrempel@users.noreply.github.com>
2020-11-29 20:24:32 -0600

    Implemented click listener for logout

--- app/src/main/java/com/pixelworks/roam/SettingsFragment.java
+++ app/src/main/java/com/pixelworks/roam/SettingsFragment.java
@@ -1,12 +1,24 @@
 package com.pixelworks.roam;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preferences, rootKey);
+
+        Preference signOut = findPreference("signOut");
+        signOut.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Log.d("TEST", "Logout button clicked.");
+
+                return true;
+            }
+        });
     }
 }
